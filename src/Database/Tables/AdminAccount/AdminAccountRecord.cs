0ca0da636836378f109d1b2813c84a456fe7f81b
Frankium <tis7bfrankie@gmail.com>
2021-05-28 13:54:55 +0800

    removed unwanted property: table wrapper

--- src/Database/Tables/AdminAccount/AdminAccountRecord.cs
+++ src/Database/Tables/AdminAccount/AdminAccountRecord.cs
@@ -26,39 +26,38 @@ namespace SecretGarden.OrderSystem.Database.Tables.AdminAccount{
 		public string firstName{
 			get=>rd_first_name;
 			set{
-				tableWrapper.update_field("first_name",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
+				table_wrapper.update_field("first_name",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
 				this.rd_first_name = value;
 			}
 		}
 		public string lastName{
 			get=>rd_last_name;
 			set{
-				tableWrapper.update_field("last_name",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
+				table_wrapper.update_field("last_name",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
 				this.rd_last_name = value;
 			}
 		}
 		public string passwordSalt{
 			get=>rd_password_salt;
 			set{
-				tableWrapper.update_field("password_salt",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
+				table_wrapper.update_field("password_salt",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
 				this.rd_password_salt = value;
 			}
 		}
 		public string passwordHash{
 			get=>rd_password_hash;
 			set{
-				tableWrapper.update_field("password_hash",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
+				table_wrapper.update_field("password_hash",value,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
 				this.rd_password_hash = value;
 			}
 		}
 		public Datetime establishDate{
 			get=>rd_establish_date;
 			set{
-				tableWrapper.update_field("establish_date",value.sqlFormatDate,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
+				table_wrapper.update_field("establish_date",value.sqlFormatDate,DBWrapper.prepare_datatypes.STRING,$"admin_id={primaryKey}");
 				this.rd_establish_date = value;
 			}
 		}
-		public DBTable tableWrapper{get=>table_wrapper;}
 		public string sqlTulpe{
 			get{return $"({this.rd_admin_id}, '{this.rd_first_name}', '{this.rd_last_name}', '{this.rd_password_salt}', '{this.rd_password_hash}', '{this.rd_establish_date.sqlFormatDate}')";}
 		}
