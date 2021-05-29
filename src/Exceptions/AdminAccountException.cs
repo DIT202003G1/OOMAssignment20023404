c22d7ca4f996229f15197573896abb4705053000
Frankium <tis7bfrankie@gmail.com>
2021-05-29 18:54:23 +0800

    add AUTHENTICATION_ERROR to AdminAccountException

--- src/Exceptions/AdminAccountException.cs
+++ src/Exceptions/AdminAccountException.cs
@@ -5,10 +5,12 @@ namespace SecretGarden.OrderSystem.Exceptions{
 	[Serializable]
 	class AdminAccountException : Exception{
 		public enum exception_type{
-			ADMIN_NOT_FOUND
+			ADMIN_NOT_FOUND,
+			AUTHENTICATION_ERROR
 		};
 		static public Dictionary<exception_type,string> exception_type_message = new Dictionary<exception_type, string>{
 			{exception_type.ADMIN_NOT_FOUND,"The admin account does not exist in the database"},
+			{exception_type.AUTHENTICATION_ERROR,"The login information is not authenticated (You might have used a wrong password)"},
 		};
 		public AdminAccountException(){}
 
