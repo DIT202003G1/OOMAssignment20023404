accbb2ec751a80cfbd765c9372a1c516a8d64fe9
Frankium <tis7bfrankie@gmail.com>
2021-05-30 16:30:40 +0800

    refactored Window

--- src/InterfaceLib/Window.cs
+++ src/InterfaceLib/Window.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 using SecretGarden.OrderSystem.InterfaceLib.Shapes;
+using SecretGarden.OrderSystem.InterfaceLib.Controls;
+using SecretGarden.OrderSystem.Misc;
 
-namespace SecretGarden.OrderSystem.InterfaceLib.Controls{
+namespace SecretGarden.OrderSystem.InterfaceLib{
 	class Window : ElementBase{
 		private string title;
+		public Dictionary<string,Label> labels = new Dictionary<string, Label>();
 		public Window(string title, int x, int y, int width, int height, ConsoleColor color){
 			this.background_color = ElementBase.to_light(color);
 			this.foreground_color = ConsoleColor.Black;
@@ -19,12 +23,10 @@ namespace SecretGarden.OrderSystem.InterfaceLib.Controls{
 			Rectangle title_plate = new Rectangle(X, Y, 1, Width, ElementBase.to_dark(background_color));
 			base_plate.draw();
 			title_plate.draw();
-			if (title.Length <= width){
-				Console.SetCursorPosition(X,Y);
-				Console.BackgroundColor = ElementBase.to_dark(background_color);
-				Console.ForegroundColor = ConsoleColor.White;
-				Console.Write(title);
-			}
+			Console.SetCursorPosition(X,Y);
+			Console.BackgroundColor = ElementBase.to_dark(background_color);
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Write(StringUtils.hide_by_max_width(title, width));
 		}
 		public string Title{
 			get=>title;
