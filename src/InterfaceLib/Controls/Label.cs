52d5f0f9021d8ebba947058b5ec1169064387894
Frankium <tis7bfrankie@gmail.com>
2021-05-30 16:31:16 +0800

    add Label

---
+++ src/InterfaceLib/Controls/Label.cs
@@ -0,0 +1,45 @@
+using System;
+
+using SecretGarden.OrderSystem.InterfaceLib.Shapes;
+using SecretGarden.OrderSystem.Misc;
+
+namespace SecretGarden.OrderSystem.InterfaceLib.Controls{
+	class Label : ControlBase{
+		string text;
+		public Label(
+			Window window_to_bind, 
+			string id, 
+			int x, 
+			int y, 
+			int width, 
+			int height, 
+			ConsoleColor color,
+			string text
+		):base(
+			window_to_bind,
+			id,
+			x,
+			y,
+			width,
+			height,
+			ElementBase.to_dark(color),
+			window_to_bind.backgroundColor
+		){
+			this.text=text;
+		}
+		public string Text{
+			get=>text;
+			set{
+				this.text = value;
+				draw();
+			}
+		}
+		public override void draw(){
+			Rectangle a = new Rectangle(absoluteX,absoluteY,height,width,backgroundColor);
+			Console.SetCursorPosition(absoluteX,absoluteY);
+			Console.BackgroundColor = backgroundColor;
+			Console.ForegroundColor = foregroundColor;
+			Console.WriteLine(StringUtils.hide_by_max_width(text, width));
+		}
+	}
+}
