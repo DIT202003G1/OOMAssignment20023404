27a2487e437423614a35a57f37a3a5658e254668
Frankium <tis7bfrankie@gmail.com>
2021-05-30 14:05:41 +0800

    add rectangle

---
+++ src/InterfaceLib/Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace SecretGarden.OrderSystem.InterfaceLib.Shapes{
+	class Rectangle : Shape{
+		public Rectangle(
+			int x,
+			int y,
+			int height,
+			int width,
+			ConsoleColor background_color
+		){
+			this.x = x;
+			this.y = y;
+			this.height = height;
+			this.width = width;
+			this.background_color = background_color;
+		}
+		public override void draw(){
+			Console.Clear();
+			for (int pointer_y = Y; pointer_y < Y+height; pointer_y++){
+				Console.SetCursorPosition(X,pointer_y);
+				Console.BackgroundColor = backgroundColor;
+				for (int pointer_x = 0; pointer_x < width; pointer_x++){
+					Console.Write(" ");
+				}
+				Console.ResetColor();
+			}
+		}
+	}
+}
