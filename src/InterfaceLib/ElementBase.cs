b2a0218f202443234637536ce67f6af9e23c7512
Frankium <tis7bfrankie@gmail.com>
2021-05-30 14:30:56 +0800

    add color conversion to ElementBase

--- src/InterfaceLib/ElementBase.cs
+++ src/InterfaceLib/ElementBase.cs
@@ -1,7 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 namespace SecretGarden.OrderSystem.InterfaceLib{
 	abstract class ElementBase : IDrawables{
+		public static Dictionary<ConsoleColor,ConsoleColor> color_type_dictionary = new Dictionary<ConsoleColor,ConsoleColor>(){
+			{ConsoleColor.DarkGray,ConsoleColor.Black},
+			{ConsoleColor.Red,ConsoleColor.DarkRed},
+			{ConsoleColor.Green,ConsoleColor.DarkGreen},
+			{ConsoleColor.Blue,ConsoleColor.DarkBlue},
+			{ConsoleColor.Yellow,ConsoleColor.DarkYellow},
+			{ConsoleColor.Magenta,ConsoleColor.DarkMagenta},
+			{ConsoleColor.Cyan,ConsoleColor.DarkCyan},
+			{ConsoleColor.White,ConsoleColor.Gray}
+		};
+		public static ConsoleColor to_dark(ConsoleColor c){
+			if (color_type_dictionary.ContainsKey(c)){
+				return color_type_dictionary[c];
+			}
+			return c;
+		}
+		public static ConsoleColor to_light(ConsoleColor c){
+			if (color_type_dictionary.ContainsValue(c)){
+				foreach(ConsoleColor i in color_type_dictionary.Keys){
+					if (color_type_dictionary[i] == c){
+						return i;
+					}
+				}
+				return c;
+			}
+			return c;
+		}
 		protected ConsoleColor background_color = ConsoleColor.White;
 		protected ConsoleColor foreground_color = ConsoleColor.Black;
 		protected int x, y, width, height;
