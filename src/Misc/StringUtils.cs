ff1daf482b4f19f319ad1773d1935fd5375e3df0
Frankium <tis7bfrankie@gmail.com>
2021-05-30 19:30:45 +0800

    fixed printables

--- src/Misc/StringUtils.cs
+++ src/Misc/StringUtils.cs
@@ -4,8 +4,8 @@ using System;
 
 namespace SecretGarden.OrderSystem.Misc{
 	static class StringUtils{
-		public static string printables = " `-=~_+[]\\;',./{}|:\"<>?!@#$%^&*()abccdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"+numbers;
 		public static string numbers = "1234567890";
+		public static string printables = " `-=~_+[]\\;',./{}|:\"<>?!@#$%^&*()abccdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"+numbers;
 		public static string hide_by_max_width(string content, int width){
 			if (content.Length <= width){
 				return content;
