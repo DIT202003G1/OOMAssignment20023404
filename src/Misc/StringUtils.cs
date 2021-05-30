e0d3f1a4b0d556ececef258ff4520f9059a44803
Frankium <tis7bfrankie@gmail.com>
2021-05-30 19:36:24 +0800

    restructured stringutils to seperate types of chars

--- src/Misc/StringUtils.cs
+++ src/Misc/StringUtils.cs
@@ -5,7 +5,13 @@ using System;
 namespace SecretGarden.OrderSystem.Misc{
 	static class StringUtils{
 		public static string numbers = "1234567890";
-		public static string printables = " `-=~_+[]\\;',./{}|:\"<>?!@#$%^&*()abccdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"+numbers;
+		public static string symbols = " `-=~_+[]\\;',./{}|:\"<>?!@#$%^&*()";
+		public static string uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		public static string lowercases = "abcdefghijklmnopqrstuvwxyz";
+
+		public static string Printables{
+			get=>numbers+symbols+uppercases+lowercases;
+		} 
 		public static string hide_by_max_width(string content, int width){
 			if (content.Length <= width){
 				return content;
