0f568ea0edf6ad44c914b610b6102e41a561d2c9
Frankium <tis7bfrankie@gmail.com>
2021-06-01 10:39:11 +0800

    add order exceptions

---
+++ src/Exceptions/OrderException.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace SecretGarden.OrderSystem.Exceptions{
+	[Serializable]
+	class OrderException : Exception{
+		public enum exception_type{
+			ORDER_NOT_FOUND,
+			ORDER_FOUND,
+		};
+		static public Dictionary<exception_type,string> exception_type_message = new Dictionary<exception_type, string>{
+			{exception_type.ORDER_FOUND,"The order does not exist in the database"},
+			{exception_type.ORDER_NOT_FOUND,"The order already exist in the database"}
+		};
+		public OrderException(){}
+
+		public OrderException(exception_type type):
+		base(exception_type_message[type]){}
+	}
+}
