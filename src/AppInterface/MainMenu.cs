3d53ce92b4e7137ac27c8de12683f5b7dc5f95ec
Frankium <tis7bfrankie@gmail.com>
2021-06-03 09:54:08 +0800

    add MainMenu Window

---
+++ src/AppInterface/MainMenu.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+using SecretGarden.OrderSystem.InterfaceLib;
+using SecretGarden.OrderSystem.InterfaceLib.Controls;
+
+namespace SecretGarden.OrderSystem.AppInterface{
+	class MainMenu : Window{
+		// (string title, int x, int y, int width, int height, ConsoleColor color)
+		public MainMenu():base("Menu", 4, 3, 32, 10, ConsoleColor.Black){
+			Label l_message = new Label(this, "Message", 2, 1, 28, 1, ConsoleColor.White, "Logged in as: ");
+			List<string> items = new List<String>(){
+				"New Order",
+				"Manage Orders",
+				"Manage Customers",
+				"Admin Settings",
+				"Logout"
+			};
+			MenuList m_menu = new MenuList(this, "Menu", 2, 3, 28, 5, ConsoleColor.White, ConsoleColor.Black, items.ToArray());
+		}
+		public override int focus(){
+			while (true){
+				int status = menu_lists["Menu"].focus();
+				if (status == 1) break;
+			}
+			return menu_lists["Menu"].Index;
+		}
+	}
+}
