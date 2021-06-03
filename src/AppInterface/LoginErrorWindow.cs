d66c74a0e7f3be1b519b05cae93de58ac4f798a6
Frankium <tis7bfrankie@gmail.com>
2021-06-03 09:29:38 +0800

    add Login Error

---
+++ src/AppInterface/LoginErrorWindow.cs
@@ -0,0 +1,19 @@
+using System;
+
+using SecretGarden.OrderSystem.Misc;
+using SecretGarden.OrderSystem.InterfaceLib;
+using SecretGarden.OrderSystem.InterfaceLib.Controls;
+
+namespace SecretGarden.OrderSystem.AppInterface{
+	class LoginErrorWindow : Window{
+		// (string title, int x, int y, int width, int height, ConsoleColor color)
+		public LoginErrorWindow(string content):base("Login Error", 4, 3, 32, 6, ConsoleColor.Red){
+			Label l_msg = new Label(this,"Message",2,1,28,1, ConsoleColor.White, StringUtils.hide_by_max_width(content, 28));
+			Button b_ok = new Button(this, "OK", 2, 3, ConsoleColor.Black, ConsoleColor.White, "  OK  ");
+		}
+		public override int focus(){
+			draw();
+			return buttons["OK"].focus();
+		}
+	}
+}
