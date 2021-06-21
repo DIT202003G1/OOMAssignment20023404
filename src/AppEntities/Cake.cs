600d9920532f48846f6eb05172648496ebb4014f
Frankium <tis7bfrankie@gmail.com>
2021-06-22 01:49:46 +0800

    add Cake

---
+++ src/AppEntities/Cake.cs
@@ -0,0 +1,11 @@
+namespace SecretGarden.OrderSystem.AppEntity{
+	class Cake : Item{
+		private int size;
+		private Cake(int id, string item_name, double item_price, int cake_size)
+			:base(id, item_name, item_price){
+				this.size = cake_size;
+			}
+		public int Size{get=>size;}
+		public override ItemType get_item_type(){return ItemType.Cake;}
+	}
+}
