0ed4e4a6dae3299739d067e2266cbe357e759e70
Frankium <tis7bfrankie@gmail.com>
2021-06-23 00:55:02 +0800

    add bill appentity

---
+++ src/AppEntities/Bill.cs
@@ -0,0 +1,40 @@
+using System;
+
+using SecretGarden.OrderSystem.Database;
+
+namespace SecretGarden.OrderSystem.AppEntity{
+	class Bill{
+		private static readonly double DISCOUNT_FACTOR = 0.95;
+		private string payment_method;
+		private bool paid;
+		private Order order;
+		public Bill(Order order){
+			this.order = order;
+			this.paid = false;
+			this.payment_method = "";
+		}
+		public string paymentMethod{
+			get=>payment_method;
+			set{
+				DBWrapper.Instance.customer_order_table.retrieve(new int[] {order.Id}).paymentMethod = value;
+				payment_method = value;
+			}
+		}
+		public bool isPaid{
+			get=>paid;
+			set{
+				DBWrapper.Instance.customer_order_table.retrieve(new int[] {order.Id}).Paid = value;
+				paid = value;
+			}
+		}
+		public double Price{
+			get{
+				double price = 0;
+				foreach (OrderItem i in order.Items)
+					price += i.Item.Price;
+				if (order.Customer.is_premium()) return Math.Round(price*DISCOUNT_FACTOR,2);
+				return price;
+			}
+		}
+	}
+}
