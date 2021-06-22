8774df0ebef5df248a7e15eec3a0c9ab69a35d64
Frankium <tis7bfrankie@gmail.com>
2021-06-22 18:17:06 +0800

    add Admin AppEntity

---
+++ src/AppEntities/Admin.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SecretGarden.OrderSystem.Database;
+using SecretGarden.OrderSystem.Database.Tables.AdminAccount;
+using SecretGarden.OrderSystem.Exceptions;
+using SecretGarden.OrderSystem.Misc;
+
+namespace SecretGarden.OrderSystem.AppEntity
+{
+	public class Admin
+	{
+		private int admin_id;
+		private string admin_first_name;
+		private string admin_last_name;
+		private string admin_password_hash;
+		private string admin_password_salt;
+		private Datetime admin_establish_date;
+
+		private int AdminId => admin_id;
+
+		private string FirstName
+		{
+			get => admin_first_name;
+			set
+			{
+				DBWrapper.Instance.admin_account_table.retrieve(new[] {admin_id}).firstName = value;
+				admin_first_name = value;
+			}
+		}
+
+		private string LastName
+		{
+			get => admin_last_name;
+			set
+			{
+				DBWrapper.Instance.admin_account_table.retrieve(new[] {admin_id}).lastName = value;
+				admin_last_name = value;
+			}
+		}
+
+		private Datetime EstablishDate{
+			get=>admin_establish_date;
+		}
+
+		private string Password
+		{
+			set
+			{
+				admin_password_salt = PasswordUtils.generate_salt();
+				admin_password_hash = PasswordUtils.hash_password(admin_password_salt, value);
+			}
+		}
+
+		private Admin(int id)
+		{
+			admin_id = id;
+
+			var record = DBWrapper.Instance.admin_account_table.retrieve(new[] {id});
+			admin_first_name = record.firstName;
+			admin_last_name = record.lastName;
+			admin_establish_date = record.establishDate;
+		}
+
+		public static Admin[] fetch_admins()
+		{
+			var adminRecords = DBWrapper.Instance.admin_account_table.get_records();
+
+			var admins = new List<Admin>();
+			foreach (var record in adminRecords)
+			{
+				admins.Append(new Admin(record.primaryKey[0]));
+			}
+
+			return admins.ToArray();
+		}
+
+		public static Admin fetch_admin(int id)
+		{
+			return new(id);
+		}
+
+		public static Admin login(int id, string password)
+		{
+			var record = DBWrapper.Instance.admin_account_table.retrieve(new[] {id});
+			if (PasswordUtils.validate_password(record.passwordSalt, password, record.passwordHash))
+				return new Admin(id);
+
+			throw new AdminAccountException();
+		}
+
+		public static int new_admin(string firstName, string lastName, string password)
+		{
+			var passwordSalt = PasswordUtils.generate_salt();
+			var passwordHash = PasswordUtils.hash_password(passwordSalt, password);
+
+			var record = new AdminAccountRecord(
+				DBWrapper.Instance.admin_account_table,
+				-1, -1,
+				firstName, lastName,
+				passwordSalt, passwordHash,
+				DateTime.Now
+			);
+
+			DBWrapper.Instance.admin_account_table.new_record(record);
+
+			var records = DBWrapper.Instance.admin_account_table.get_records();
+			return records[records.ToArray().Length - 1].primaryKey[0];
+		}
+	}
+}
