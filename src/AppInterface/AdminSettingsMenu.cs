355a1b0d42678aef1d716e9319db3c8bbf999e26
Frankium <tis7bfrankie@gmail.com>
2021-06-27 21:52:54 +0800

    fixed position and color

--- src/AppInterface/AdminSettingsMenu.cs
+++ src/AppInterface/AdminSettingsMenu.cs
@@ -6,7 +6,7 @@ using SecretGarden.OrderSystem.InterfaceLib.Controls;
 
 namespace SecretGarden.OrderSystem.AppInterface{
 	class AdminSettingsMenu : Window{
-		public AdminSettingsMenu():base("Admin Settings", 10, 5, 32, 8, ConsoleColor.Black){
+		public AdminSettingsMenu():base("Admin Settings", 2, 1, 32, 8, ConsoleColor.Black){
 			List<string> items = new List<String>(){
 				"New Admin",
 				"Delete Admin",
@@ -17,9 +17,10 @@ namespace SecretGarden.OrderSystem.AppInterface{
 			MenuList m_menu = new MenuList(this, "Menu", 2, 1, 28, 5, ConsoleColor.White, ConsoleColor.Black, items.ToArray());
 		}
 		public override ConsoleKey focus(){
-			Console.Clear();
-			draw();
 			while (true){
+				Console.ResetColor();
+				Console.Clear();
+				draw();
 				ConsoleKey status = menu_lists["Menu"].focus();
 				if (status == ConsoleKey.Enter) break;
 			}
