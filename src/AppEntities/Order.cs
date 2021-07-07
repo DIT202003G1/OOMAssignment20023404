e01fe1e8c2b7e6835365dbd8952cbc0a9482490f
Frankium <tis7bfrankie@gmail.com>
2021-07-08 06:27:22 +0800

    fix wrong customer in order constructor

--- src/AppEntities/Order.cs
+++ src/AppEntities/Order.cs
@@ -28,7 +28,7 @@ namespace SecretGarden.OrderSystem.AppEntities{
 			delivery = record.isDelivery;
 			completed = record.Completed;
 			admin = Admin.fetch_admin(admin_record.primaryKey[0]);
-			customer = Customer.fetch_customer(admin_record.primaryKey[0]);
+			customer = Customer.fetch_customer(customer_record.primaryKey[0]);
 
 			bill = new Bill(this);
 
