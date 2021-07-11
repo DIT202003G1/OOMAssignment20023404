861a5e22718d5ad5ff967179c390c824ad14731b
Frankium <tis7bfrankie@gmail.com>
2021-07-11 10:25:57 +0800

    made main menu work

--- src/AppInterface/MainMenu.cs
+++ src/AppInterface/MainMenu.cs
@@ -2,13 +2,15 @@ using System;
 using System.Collections.Generic;
 
 using SecretGarden.OrderSystem.InterfaceLib;
+using SecretGarden.OrderSystem.AppEntities;
 using SecretGarden.OrderSystem.InterfaceLib.Controls;
 
 namespace SecretGarden.OrderSystem.AppInterface{
 	class MainMenu : Window{
-		// (string title, int x, int y, int width, int height, ConsoleColor color)
-		public MainMenu():base("Menu", 2, 1, 32, 10, ConsoleColor.Black){
-			Label l_message = new Label(this, "Message", 2, 1, 28, 1, ConsoleColor.White, "Logged in as: ");
+		Admin admin;
+		public MainMenu(Admin admin):base("Menu", 2, 1, 32, 10, ConsoleColor.Black){
+			this.admin = admin;
+			Label l_message = new Label(this, "Message", 2, 1, 28, 1, ConsoleColor.White, $"Logged in as: {admin.firstName} {admin.lastName}");
 			List<string> items = new List<String>(){
 				"New Order",
 				"Manage Orders",
@@ -24,9 +26,39 @@ namespace SecretGarden.OrderSystem.AppInterface{
 				Console.Clear();
 				draw();
 				ConsoleKey status = menu_lists["Menu"].focus();
-				if (status == ConsoleKey.Enter) break;
+				if (status == ConsoleKey.Enter) {
+					Console.SetCursorPosition(0,0);
+					Console.WriteLine(menu_lists["Menu"].Index);
+					switch (menu_lists["Menu"].Index){
+						case 0:
+							NewOrderMenu nom = new NewOrderMenu(this.admin);
+							nom.focus();
+							break;
+						case 1:
+							OrderSelectionMenu osm = new OrderSelectionMenu();
+							osm.focus();
+							break;
+						case 2:
+							while (true){
+								SelectCustomerMenu scm = new SelectCustomerMenu(true);
+								if (scm.focus() == ConsoleKey.Enter){
+									Customer customer = Customer.fetch_customers()[scm.menu_lists["customers_list"].Index];
+									ManageCustomerAccountMenu mcam = new ManageCustomerAccountMenu(customer);
+									mcam.focus();
+									continue;
+								}
+								else break;
+							}
+							break;
+						case 3:
+							AdminSettingsMenu asm = new AdminSettingsMenu();
+							asm.focus();
+							break;
+						case 4:
+							return ConsoleKey.Enter;
+					}
+				};
 			}
-			return ConsoleKey.Enter;
 		}
 	}
 }
